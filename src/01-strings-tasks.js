02d5d324e42934b311111d49e0bba147cb471bae
olce_gabbana <Olya.Sokolova.241902@yandex.ru>
2024-01-08 23:56:38 +0300

    ver2

--- src/01-strings-tasks.js
+++ src/01-strings-tasks.js
@@ -176,7 +176,7 @@ function convertToUpperCase(str) {
  *   'info@gmail.com' => ['info@gmail.com']
  */
 function extractEmails(/* str */) {
-  // return 
+  throw new Error('Not implemented');
 }
 
 /**
